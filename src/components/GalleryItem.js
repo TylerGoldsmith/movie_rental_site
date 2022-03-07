7716cac27401f60dfa7f877342c9c91b7e686d2b
TylerGoldsmith <tyler.r.goldsmith@gmail.com>
2022-03-07 17:19:13 -0500

    Added Movie Details component

--- src/components/GalleryItem.js
+++ src/components/GalleryItem.js
@@ -1,10 +1,11 @@
+// dependencies
+import react from 'react';
+// components
+import MovieDetails from './MovieDetails'
+
 const GalleryItem = () => {
   return(
-      <div>
-        <h1>Movie Details here</h1>
-        <p><a href="../App2.js">Movie Page</a></p>
-        {/* replace href with query using db */}
-      </div>
+      <div><MovieDetails /></div>
   )
 };
 export default GalleryItem;
