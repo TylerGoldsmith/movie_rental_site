944cbbf82cd6fb2cbc7bd96bb0a0053ab06030c1
TylerGoldsmith <tyler.r.goldsmith@gmail.com>
2022-03-07 17:23:00 -0500

    Make initial Movie Details page

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -1,4 +1,14 @@
-const MovieDetails = () => {
-  return;
+// dependencies
+import react from 'react';
+
+
+function MovieDetails(props) {
+  return(
+    <div>
+    <h1>Movie Details here</h1>
+    <p><a href="../App2.js">Movie Page</a></p>
+    {/* replace href with query using db */}
+  </div>
+    )
 };
 export default MovieDetails;
