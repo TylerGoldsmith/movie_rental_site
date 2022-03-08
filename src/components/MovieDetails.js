953318fdbb78735ca1d6041ab2fcba45bfe99159
TylerGoldsmith <tyler.r.goldsmith@gmail.com>
2022-03-07 21:26:11 -0500

    have ActorDetails interact with database

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -5,8 +5,9 @@ function MovieDetails(props) {
   return(
     <div>
     <h1>Movie Details here</h1>
-    <p><a href="/movie/`${movie_id}`">Movie Page</a></p>
+
     {/* replace href with query using db */}
+    {/* add movie actors component */}
   </div>
     )
 };
