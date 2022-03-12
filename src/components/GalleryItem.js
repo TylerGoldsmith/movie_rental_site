fb3678d0bca3e3eefb51c1188b9eda43daf63d9f
KevinsCodeStorage <89494039+KevinsCodeStorage@users.noreply.github.com>
2022-03-12 09:26:23 -0500

    component changes

--- src/components/GalleryItem.js
+++ src/components/GalleryItem.js
@@ -1,17 +1,16 @@
 // dependencies
 // components
-import MovieDetails from './MovieDetails'
+import MovieDetails from "./MovieDetails";
+import { Link } from "react-router-dom";
 
-const GalleryItem = () => {
-  return(
-      <div>
-        <h1>
-          Gallery Item Test
-          </h1>
-          <MovieDetails key={i}/>
-          {/* <p><a href=`/movie/${movie_id}`>Movie Page</a></p> */}
-          
-          </div>
-  )
+const GalleryItem = (props) => {
+  return (
+    <div>
+      <h1>Gallery Item Test</h1>
+      {props.title}
+      <Link to={`/movie/:${props.movie_id}`}></Link>
+      {/* <p><a href=`/movie/${movie_id}`>Movie Page</a></p> */}
+    </div>
+  );
 };
 export default GalleryItem;
