c6d2746d249b8204f0174a2732756f6acdafe992
KevinsCodeStorage <89494039+KevinsCodeStorage@users.noreply.github.com>
2022-03-12 11:01:44 -0500

    Update GalleryItem.js

--- src/components/GalleryItem.js
+++ src/components/GalleryItem.js
@@ -2,9 +2,8 @@
 import { Link } from 'react-router-dom'
 // components
 import MovieDetails from "./MovieDetails";
-import { Link } from "react-router-dom";
 
-const GalleryItem = () => {
+const GalleryItem = (props) => {
   return(
       <div>
           <h1>Gallery Item Test</h1>
